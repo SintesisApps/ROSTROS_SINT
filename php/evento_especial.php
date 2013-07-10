650f46b9233bb008a4f64421b12bcdee163d0fc4
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-07-10 13:28:42 -0500

    video rostros

---
+++ php/evento_especial.php
@@ -0,0 +1,103 @@
+<?php
+
+echo'
+
+
+
+<link rel = "stylesheet" href = "css/default.css" />
+		<link rel = "stylesheet" media = "screen" href = "css/common.css" />
+		
+		<!-- JS -->
+		<script src="js/jquery-1.9.1.min.js"></script>
+<script src="js/jquery.mobile-1.3.1.min.js"></script>
+		
+		<script src = "js/jquery.easing-1.3.js"></script>
+		<script src = "js/jquery.iosslider.js"></script>
+		<script src = "js/common.js"></script>
+		
+		<script>
+		/*function animacion()
+		{
+			alert("animacion");	
+			//document.getElementById("r_descripcion").style.display="none";
+			//$("#r_descripcion").animate({width:"toggle"});
+		}*/
+/*$(document).ready(function() {
+
+
+	$("#r_flecha1").click(function(){
+		alert("ljlljl");
+   // $("#r_descripcion").animate({width:"toggle"});
+  });*/
+  
+
+	
+
+</script>
+		
+<div id="foto_rostros" class="foto_rostros">
+        	<div class = "snap-slider">
+		
+			<div class = "slider">
+			
+				<div class = "item" id = "item1"><img src="img/fotos/5.jpg" /></div>
+				<div class = "item" id = "item1"><img src="img/fotos/4.jpg" /></div>
+                <div class = "item" id = "item1"><img src="img/fotos/3.jpg" /></div>
+                <div class = "item" id = "item1"><img src="img/fotos/2.jpg" /></div>
+                <div class = "item" id = "item1"><img src="img/fotos/1.jpg" /></div>
+			</div>
+		
+			</div>	
+        </div>
+        <div id="descripcion_rostros" class="descripcion_rostros">
+        	<div id="r_descripcion" class="r_descripcion">
+            	<div id="r_name" class="r_name" >
+                	<span class="r_nombre"> ALEJANDRO</span><br>
+                    <span class="r_apellido" > OCHOA</span>
+                </div>
+                <div class="r_separador"></div>
+            	<div class="r_texto">
+                	<span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                   <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br><span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+               
+                </div>
+            </div>
+        	<div id="r_flecha" class="r_flecha">
+            <a href="javascript:;" id="r_flecha1">
+        <img src="img/iconos_app/indicador.png" />
+         </a>
+            </div>
+        </div>
+		<script>
+		
+$(document).ready(function() {
+
+
+	$("#r_flecha1").click(function(){
+		alert("ljlljl");
+   // $("#r_descripcion").animate({width:"toggle"});
+  });
+  
+
+	
+
+</script>
+
+';
+
+?>
---
+++ php/secciones.php
@@ -0,0 +1,66 @@
+<?php
+echo '
+
+
+
+
+	<link rel = "stylesheet" href = "css/default.css" />
+		<link rel = "stylesheet" media = "screen" href = "css/common.css" />
+		
+		<!-- JS -->
+		<script src = "js/jquery.easing-1.3.js"></script>
+		<script src = "js/jquery.iosslider.js"></script>
+		<script src = "js/common.js"></script>
+		
+		<!-- slidesec -->
+
+  <div id="foto_seccion" class="foto_seccion">
+        	<div class = "snap-slider">
+		
+			<div class = "slider slider_seccion">
+			
+				<div class = "item" id = "item1"><div id="flecha_mas" ><img src="img/iconos_app/mas@2x.png" width="68" height="68"></div><img src="img/bystyle.jpg" /></div>
+				<div class = "item" id = "item1"><img src="img/conoce.jpg" /></div>
+                <div class = "item" id = "item1"><img src="img/consientete.jpg" /></div>
+                <div class = "item" id = "item1"><img src="img/disfruta.jpg" /></div>
+			</div>
+		
+			</div>	
+        </div>
+        <div id="descripcion_seccion" class="descripcion_seccion">
+        	<div id="s_descripcion" class="s_descripcion">
+            	<div id="s_name" class="s_name" >
+                	<span class="s_titulo"> HIDRATA</span><br>
+                    <span class="s_subtitulo" > TU PIEL</span>
+                </div>
+                <div class="s_separador"></div>
+            	<div class="s_texto">
+                	<span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                   <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br><span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+                    <span class="r_pregunta"> texto texto texto texto  texto texto:</span><br>
+                    <span class="r_respuesta">texto texto texto texto  texto textotexto texto texto texto  texto texto</span><br><br>
+               
+                </div>
+            </div>
+        	<div id="r_flecha" class="r_flecha">
+            <a href="javascript:;" id="r_flecha2">
+        <img src="img/iconos_app/indicador.png" />
+         </a>
+            </div>
+        </div>
+';
+
+?>
